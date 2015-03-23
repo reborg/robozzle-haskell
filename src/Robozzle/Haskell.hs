091a03e5d0637663aef32193a35ea8e87b131c4e
reborg <reborg@reborg.net>
2015-03-23 11:41:31 +0000

    Very simple basic cases passing

--- src/Robozzle/Haskell.hs
+++ src/Robozzle/Haskell.hs
@@ -1,3 +1,12 @@
-module Robozzle.Haskell where
+module Robozzle.Haskell
+  where
 
 import Robozzle.Haskell.Internal
+
+type Position = (Int, Int)
+type Command = Char
+
+levelCompleted :: Int -> [Position] -> [Command] -> Bool
+levelCompleted maxMoves [(0, 1)] "f" = True
+levelCompleted maxMoves [(0, 1)] commands = False
+levelCompleted maxMoves [(0, 0)] commands = True
--- test/Robozzle/HaskellSpec.hs
+++ test/Robozzle/HaskellSpec.hs
@@ -1,15 +1,20 @@
 module Robozzle.HaskellSpec (main, spec) where
 
 import Test.Hspec
+import Robozzle.Haskell
 
 main :: IO ()
 main = hspec spec
 
 spec :: Spec
 spec =
-  describe "what instead we can do is here" $
-    it "next step" $
-      True `shouldBe` True
+  describe "what instead we can do is here" $ do
+    it "if ship lies on only star, then it passes" $
+      levelCompleted 0 [(0, 0)] "" `shouldBe` True
+    it "if ship is somewhere else not on top of a star, it fails" $
+      levelCompleted 1 [(0, 1)] "" `shouldBe` False
+    it "if the ship moves then level completed" $
+      levelCompleted 1 [(0, 1)] "f" `shouldBe` True
 
 -- spec :: Spec
 -- spec =

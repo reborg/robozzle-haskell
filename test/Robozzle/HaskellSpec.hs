7e48b5e7013d0c9371c7950ae63bced4265df8ab
reborg <reborg@reborg.net>
2015-03-23 21:36:50 +0000

    Main acceptance now passing

--- test/Robozzle/HaskellSpec.hs
+++ test/Robozzle/HaskellSpec.hs
@@ -32,13 +32,11 @@ spec = do
       nextState (ShipState (0,1) (0,0) "llff") `shouldBe` ShipState (-1,0) (0,0) "lff"
       nextState (ShipState (-1,0) (0,0) "llff") `shouldBe` ShipState (0,-1) (0,0) "lff"
 
--- spec :: Spec
--- spec =
---   describe "whishful programming, how the real thing should look like, but it's too big" $
---     it "level completion rules" $
---       levelCompleted withinMaxNumberOfMoves starsPositions shipCommands `shouldBe` True
---       levelCompleted (withinMaxNumberOfMoves - 1) starsPositions shipCommands `shouldBe` True
---         where
---           withinMaxNumberOfMoves = 9
---           starsPositions = [(-2, 0), (2, 0)]
---           shipCommands = "rffllffff"
+  describe "whishful programming, how the real thing should look like, but it's too big" $ do
+    it "level completion rules" $ do
+      levelCompleted withinMaxNumberOfMoves starsPositions shipCommands `shouldBe` True
+      levelCompleted (withinMaxNumberOfMoves - 1) starsPositions shipCommands `shouldBe` False
+        where
+          withinMaxNumberOfMoves = 9
+          starsPositions = [(-2, 0), (2, 0)]
+          shipCommands = "rffllffff"

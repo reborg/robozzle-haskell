d4d924dafdc5f5464a919b558a00b8dba747296e
reborg <reborg@reborg.net>
2015-03-23 14:09:58 +0000

    Replaceing match-all with proper param names

--- src/Robozzle/Haskell.hs
+++ src/Robozzle/Haskell.hs
@@ -8,6 +8,6 @@ type Position = (Int, Int)
 type Command = Char
 
 levelCompleted :: Int -> [Position] -> [Command] -> Bool
-levelCompleted _ [(0, 1)] "f" = null ([(0,1)] \\ [(0,0),(0,1)])
-levelCompleted _ [(0, 1)] "" = null ([(0,1)] \\ [(0,0)])
-levelCompleted _ [(0, 0)] "" = null ([(0,0)] \\ [(0,0)])
+levelCompleted _ ps "f" = null (ps \\ [(0,0),(0,1)])
+levelCompleted _ ps "" = null (ps \\ [(0,0)])
+levelCompleted _ ps "" = null (ps \\ [(0,0)])

da7883bbdeba9d8b7788ab3b44294c5922341f6e
reborg <reborg@reborg.net>
2015-03-23 14:13:39 +0000

    Extract fn shiptrail from constant path

--- src/Robozzle/Haskell.hs
+++ src/Robozzle/Haskell.hs
@@ -8,5 +8,8 @@ type Position = (Int, Int)
 type Command = Char
 
 levelCompleted :: Int -> [Position] -> [Command] -> Bool
-levelCompleted _ ps "f" = null (ps \\ [(0,0),(0,1)])
+levelCompleted _ ps "f" = null (ps \\ shipTrail "f")
 levelCompleted _ ps "" = null (ps \\ [(0,0)])
+
+shipTrail :: [Command] -> [Position]
+shipTrail "f" = [(0,0),(0,1)]

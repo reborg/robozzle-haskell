4d6b1bfb2dc0ae61f94a51853a1b586abc088959
reborg <reborg@reborg.net>
2015-03-23 15:06:38 +0000

    Factored out position fn and a Data called ShipState

--- src/Robozzle/Haskell.hs
+++ src/Robozzle/Haskell.hs
@@ -4,6 +4,8 @@ module Robozzle.Haskell
 import Robozzle.Haskell.Internal
 import Data.List
 
+data ShipState = ShipState Direction Position [Command]
+type Direction = (Int, Int)
 type Position = (Int, Int)
 type Command = Char
 
@@ -11,12 +13,17 @@ levelCompleted :: Int -> [Position] -> [Command] -> Bool
 levelCompleted n ps cs = null (ps \\ take (n + 1) (shipTrail cs))
 
 shipTrail :: [Command] -> [Position]
-shipTrail "f" = [(0,0),(0,1)]
+-- shipTrail "f" = [(0,0),(0,1)]
+shipTrail "f" = map position [ShipState (0,1) (0,0) "f", ShipState (0,1) (0,1) ""]
 shipTrail "" = [(0,0)]
+
+position :: ShipState -> Position
+position (ShipState _ p _) = p
 
 {-
 - take aways so far:
 - nested ifs or conds are not used, pattern matching on specific constant is preferred
 - pattern match are used from the most general _ matchall
 - 'fake' _ matchall are then gradually removed, test by test, accounting for more conditions
+- implementation is removing "constants" that are only good during initial phase
 -}
